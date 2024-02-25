dececaf0911291f0b6ea75eb2edfd30ea57709e7
Meeroj <sirojiddinovmerojiddin@gmail.com>
2024-02-25 18:45:22 +0500

    All done

---
+++ main.js
@@ -0,0 +1,8 @@
+document.getElementById('menuResponsive').addEventListener('click', function() {
+    var navLinks = document.getElementById('navLinks');
+    if (navLinks.style.display === 'block') {
+        navLinks.style.display = 'none';
+    } else {
+        navLinks.style.display = 'block';
+    }
+});
